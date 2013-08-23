c7589c611dcb4b76a81ef001263172b712adffa6
2033662611 <2033662611@qq.com>
2013-08-23 10:35:18 +0800

    open users add add action users login and users logout add view users login hash password in model user beforeSave

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,53 +1,72 @@
 <?php
-App::uses('AppModel', 'Model');
+App::uses ( 'AppModel', 'Model' );
+App::uses ( 'AuthComponent', 'Controller/Component' );
 /**
  * User Model
- *
  */
 class User extends AppModel {
-
-/**
- * Display field
- *
- * @var string
- */
+	
+	/**
+	 * Display field
+	 *
+	 * @var string
+	 */
 	public $displayField = 'username';
-
-/**
- * Validation rules
- *
- * @var array
- */
-	public $validate = array(
-		'username' => array(
-			'notempty' => array(
-				'rule' => array('notempty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+	
+	/**
+	 * Validation rules
+	 *
+	 * @var array
+	 */
+	public $validate = array (
+			'username' => array (
+					'notempty' => array (
+							'rule' => array (
+									'notempty' 
+							) 
+					// 'message' => 'Your custom message here',
+					// 'allowEmpty' => false,
+					// 'required' => false,
+					// 'last' => false, // Stop validation after this rule
+					// 'on' => 'create', // Limit validation to 'create' or 'update' operations
+										) 
 			),
-		),
-		'password' => array(
-			'notempty' => array(
-				'rule' => array('notempty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			'password' => array (
+					'notempty' => array (
+							'rule' => array (
+									'notempty' 
+							) 
+					// 'message' => 'Your custom message here',
+					// 'allowEmpty' => false,
+					// 'required' => false,
+					// 'last' => false, // Stop validation after this rule
+					// 'on' => 'create', // Limit validation to 'create' or 'update' operations
+										) 
 			),
-		),
-		'role' => array(
-			'inlist' => array(
-				'rule' => array('inlist', array('admin', 'author')),
-				'message' => 'Please enter a valid role',
-				'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
+			'role' => array (
+					'inlist' => array (
+							'rule' => array (
+									'inlist',
+									array (
+											'admin',
+											'author' 
+									) 
+							),
+							'message' => 'Please enter a valid role',
+							'allowEmpty' => false 
+					// 'required' => false,
+					// 'last' => false, // Stop validation after this rule
+					// 'on' => 'create', // Limit validation to 'create' or 'update' operations
+										) 
+			) 
 	);
+	
+	/**
+	 */
+	public function beforeSave($options = array()) {
+		if (isset ( $this->data [$this->alias] ['password'] )) {
+			$this->data [$this->alias] ['password'] = AuthComponent::password ( $this->data [$this->alias] ['password'] );
+		}
+		return true;
+	}
 }
